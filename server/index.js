ace61c566f4d4b0634fa9a5d27985eb9e517c816
deepranjankumar <deepranjankumar@120702@gmail.com>
2024-08-19 18:14:39 +0530

    -a

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const http = require('http');
 const { Server } = require("socket.io");
 
 const authRoutes = require('./Routes/AuthRoutes');
-
+mongoose.set('strictQuery', false);
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
