9727547799c016daeaa255d23ed1d7cb6c326dd2
deepranjankumar <114337290+deepranjankumar@users.noreply.github.com>
2024-08-19 17:34:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // Ensure this matches your frontend's URL
+    origin: "https://medicine-service-development-4.onrender.com/", // Ensure this matches your frontend's URL
     methods: ["GET", "POST"],
     credentials: true
   }
