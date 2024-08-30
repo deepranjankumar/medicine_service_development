04c4f0b561edea311d129819545d6872589f6880
deepranjankumar <114337290+deepranjankumar@users.noreply.github.com>
2024-08-31 00:08:57 +0530

    Update authControllers.js

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -40,7 +40,7 @@ try {
     const { email, password } = req.body;
     const user = await User.create({ email, password });
     const token = createToken(user._id);
-
+     localStorage.setItem("jwtoken", token);
  res.cookie("jwt", token, {
   httpOnly: true, // Ensure this is set for security
   secure: process.env.NODE_ENV === 'production', // true if using HTTPS
